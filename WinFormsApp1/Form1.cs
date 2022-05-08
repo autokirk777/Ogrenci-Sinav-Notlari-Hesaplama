dfdcf81209c108b2d20b90bad19a314dc788c12c
burak002 <93756463+burak002@users.noreply.github.com>
2022-05-08 17:51:05 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad, ogrencinumarasi;
+            double ortalama, sinav1, sinav2, proje,sinav1agirlik, sinav2agirlik, projeagirlik;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            ogrencinumarasi = maskedTextBox1.Text;
+            sinav1 = Convert.ToDouble(textBox3.Text);
+            sinav2 = Convert.ToDouble(textBox4.Text);
+            proje = Convert.ToDouble(textBox5.Text);
+            sinav1agirlik = Convert.ToDouble(textBox7.Text);
+            sinav2agirlik = Convert.ToDouble(textBox8.Text);
+            projeagirlik = Convert.ToDouble(textBox9.Text);
+            ortalama = (sinav1 * sinav1agirlik) + (sinav2 * sinav2agirlik) + (proje * projeagirlik);
+            listBox1.Items.Add(ad + " " + soyad + " " + ogrencinumarasi + " " + "Ortalama: " + ortalama);
+        }
+    }
+}
